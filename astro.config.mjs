f79e2734921c395ca1511a8ead2eeb4bfe949f3a
Trenton Bien <trenton.bien@gmail.com>
2026-07-02 23:19:50 -0700

    picture update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tbien722.github.io',
-  base: '/The-Cardist',
+  base: '/The-Cardist/',
 });
